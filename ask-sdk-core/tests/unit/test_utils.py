acba478dd60b6493decbf3458bd20834e8beb108
Nikhil Yogendra Murali <nnky@amazon.com>
2019-03-14 03:56:33 -0700

    Add request utility methods
    This commit introduces some commonly used request utility methods
    like get_locale, get_intent_name etc.

--- ask-sdk-core/tests/unit/test_utils.py
+++ ask-sdk-core/tests/unit/test_utils.py
@@ -19,13 +19,20 @@ import unittest
 import random
 
 from ask_sdk_model import (
-    IntentRequest, RequestEnvelope, Intent, SessionEndedRequest, Context)
+    IntentRequest, RequestEnvelope, Intent, SessionEndedRequest, Context,
+    LaunchRequest, DialogState, Slot, Session, User, Device,
+    SupportedInterfaces)
 from ask_sdk_model.canfulfill import CanFulfillIntentRequest
+from ask_sdk_model.interfaces.viewport import ViewportState, Shape
+from ask_sdk_model.interfaces.system import SystemState
+from ask_sdk_model.interfaces.display import DisplayInterface
 from ask_sdk_core.utils import (
-    is_canfulfill_intent_name, is_intent_name, is_request_type, viewport)
+    is_canfulfill_intent_name, is_intent_name, is_request_type, viewport,
+    get_slot, get_slot_value, get_account_linking_access_token,
+    get_api_access_token, get_device_id, get_dialog_state, get_intent_name,
+    get_locale, get_request_type, is_new_session, get_supported_interfaces)
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.exceptions import AskSdkException
-from ask_sdk_model.interfaces.viewport import ViewportState, Shape
 
 
 def test_is_canfulfill_intent_name_match():
@@ -393,3 +400,236 @@ class TestViewportProfile(unittest.TestCase):
         assert (viewport.get_viewport_profile(test_request_env)
                 is viewport.ViewportProfile.UNKNOWN_VIEWPORT_PROFILE), (
             "Viewport profile couldn't resolve UNKNOWN_VIEWPORT_PROFILE")
+
+
+class TestRequestUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.test_locale = "foo_locale"
+        self.test_request_type = "LaunchRequest"
+        self.test_dialog_state = DialogState.COMPLETED
+        self.test_intent_name = "foo_intent"
+        self.test_slot_name = "foo_slot"
+        self.test_slot_value = "foo_slot_value"
+        self.test_slot = Slot(
+            name=self.test_slot_name, value=self.test_slot_value)
+        self.test_api_access_token = "foo_api_access_token"
+        self.test_access_token = "foo_account_linking_access_token"
+        self.test_device_id = "foo_device_id"
+        self.test_supported_interfaces = SupportedInterfaces(
+            display=DisplayInterface(
+                template_version="test_template", markup_version="test_markup")
+        )
+        self.test_new_session = False
+
+        self.test_launch_request = LaunchRequest(locale=self.test_locale)
+        self.test_intent_request = IntentRequest(
+            dialog_state=self.test_dialog_state,
+            intent=Intent(
+                name=self.test_intent_name,
+                slots={
+                    self.test_slot_name: Slot(
+                        name=self.test_slot_name,
+                        value=self.test_slot_value)
+                }))
+        self.test_request_envelope = RequestEnvelope(
+            session=Session(new=self.test_new_session),
+            context=Context(
+                system=SystemState(
+                    user=User(access_token=self.test_access_token),
+                    api_access_token=self.test_api_access_token,
+                    device=Device(
+                        device_id=self.test_device_id,
+                        supported_interfaces=self.test_supported_interfaces))))
+
+    def _create_handler_input(self, request):
+        self.test_request_envelope.request = request
+        return HandlerInput(request_envelope=self.test_request_envelope)
+
+    def test_get_locale(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_locale(handler_input=test_input), self.test_locale,
+            "get_locale method returned incorrect locale for input request")
+
+    def test_get_request_type(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_request_type(handler_input=test_input), self.test_request_type,
+            "get_request_type method returned incorrect request type for "
+            "input request")
+
+    def test_get_intent_name_throws_exception_for_non_intent_request(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        with self.assertRaises(
+                TypeError,
+                msg="get_intent_name method didn't throw TypeError when an "
+                    "invalid request type is passed"):
+            get_intent_name(handler_input=test_input)
+
+    def test_get_intent_name(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_intent_name(handler_input=test_input), self.test_intent_name,
+            "get_intent_name method returned incorrect intent name for "
+            "input request")
+
+    def test_get_account_linking_access_token(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_account_linking_access_token(handler_input=test_input),
+            self.test_access_token,
+            "get_account_linking_access_token method returned incorrect "
+            "access token from input request")
+
+    def test_get_api_access_token(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_api_access_token(handler_input=test_input),
+            self.test_api_access_token,
+            "get_api_access_token method returned incorrect "
+            "api access token from input request")
+
+    def test_get_device_id(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_device_id(handler_input=test_input),
+            self.test_device_id,
+            "get_device_id method returned incorrect "
+            "device id from input request")
+
+    def test_get_dialog_state_throws_exception_for_non_intent_request(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        with self.assertRaises(
+                TypeError,
+                msg="get_dialog_state method didn't throw TypeError when an "
+                    "invalid request type is passed"):
+            get_dialog_state(handler_input=test_input)
+
+    def test_get_dialog_state(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_dialog_state(handler_input=test_input),
+            self.test_dialog_state,
+            "get_dialog_state method returned incorrect "
+            "dialog state from input request")
+
+    def test_get_slot_throws_exception_for_non_intent_request(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        with self.assertRaises(
+                TypeError,
+                msg="get_slot method didn't throw TypeError when an "
+                    "invalid request type is passed"):
+            get_slot(handler_input=test_input, slot_name=self.test_slot_name)
+
+    def test_get_slot_returns_none_for_no_slots(self):
+        self.test_intent_request.intent.slots = None
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_slot(handler_input=test_input, slot_name="some_slot"),
+            None,
+            "get_slot method didn't return None from input request "
+            "when intent request has no slots")
+
+    def test_get_slot_returns_none_for_non_existent_slot(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_slot(handler_input=test_input, slot_name="some_slot"),
+            None,
+            "get_slot method didn't return None from input request "
+            "when a non-existent slot name is passed")
+
+    def test_get_slot(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_slot(handler_input=test_input, slot_name=self.test_slot_name),
+            self.test_slot,
+            "get_slot method returned incorrect slot from input request "
+            "when a valid slot name is passed")
+
+    def test_get_slot_value_throws_exception_for_non_intent_request(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        with self.assertRaises(
+                TypeError,
+                msg="get_slot_value method didn't throw TypeError when an "
+                    "invalid request type is passed"):
+            get_slot_value(
+                handler_input=test_input, slot_name=self.test_slot_name)
+
+    def test_get_slot_value_throws_exception_for_non_existent_slot(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        with self.assertRaises(
+                ValueError,
+                msg="get_slot_value method didn't throw ValueError when an "
+                    "invalid slot name is passed"):
+            get_slot_value(handler_input=test_input, slot_name="some_slot")
+
+    def test_get_slot_value(self):
+        test_input = self._create_handler_input(
+            request=self.test_intent_request)
+
+        self.assertEqual(
+            get_slot_value(
+                handler_input=test_input, slot_name=self.test_slot_name),
+            self.test_slot_value,
+            "get_slot_value method returned incorrect slot value from "
+            "input request when a valid slot name is passed")
+
+    def test_get_supported_interfaces(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            get_supported_interfaces(handler_input=test_input),
+            self.test_supported_interfaces,
+            "get_supported_interfaces method returned incorrect "
+            "supported interfaces from input request")
+
+    def test_is_new_session_throws_exception_if_session_not_exists(self):
+        test_input = HandlerInput(request_envelope=RequestEnvelope())
+
+        with self.assertRaises(
+                TypeError,
+                msg="is_new_session method didn't throw TypeError when an "
+                    "input request without session is passed"):
+            is_new_session(handler_input=test_input)
+
+    def test_is_new_session(self):
+        test_input = self._create_handler_input(
+            request=self.test_launch_request)
+
+        self.assertEqual(
+            is_new_session(handler_input=test_input),
+            self.test_new_session,
+            "is_new_session method returned incorrect session information "
+            "from input request when a session exists")
