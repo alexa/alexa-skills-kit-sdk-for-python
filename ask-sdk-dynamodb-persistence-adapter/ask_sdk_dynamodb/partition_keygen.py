80dba6e961069dc42e0fd0a48fb0047af69c04a6
Nikhil Yogendra Murali <25237515+nikhilym@users.noreply.github.com>
2019-11-18 15:00:25 -0800

    Add personId partitionKey generator (#125)
    * Add personId partitionKey generator

--- ask-sdk-dynamodb-persistence-adapter/ask_sdk_dynamodb/partition_keygen.py
+++ ask-sdk-dynamodb-persistence-adapter/ask_sdk_dynamodb/partition_keygen.py
@@ -59,3 +59,32 @@ def device_id_partition_keygen(request_envelope):
     except AttributeError:
         raise PersistenceException("Couldn't retrieve device id from "
                                    "request envelope, for partition key use")
+
+
+def person_id_partition_keygen(request_envelope):
+    # type: (RequestEnvelope) -> str
+    """Retrieve person id from request envelope, to use as object key.
+
+    This method retrieves the `person id` specific to a voice profile from
+    the request envelope if it exists, to be used as an object key. If it
+    doesn't exist, then the `user id` is returned instead.
+
+    :param request_envelope: Request Envelope passed during skill
+        invocation
+    :type request_envelope: ask_sdk_model.RequestEnvelope
+    :return: person Id retrieved from request envelope if exists, else
+        fall back on User Id
+    :rtype: str
+    :raises: :py:class:`ask_sdk_core.exceptions.PersistenceException`
+    """
+    try:
+        person_id = request_envelope.context.system.person.person_id
+    except AttributeError:
+        try:
+            person_id = user_id_partition_keygen(
+                request_envelope=request_envelope)
+        except PersistenceException:
+            raise PersistenceException(
+                "Couldn't retrieve person id or user id from request envelope, "
+                "for partition key use")
+    return person_id
